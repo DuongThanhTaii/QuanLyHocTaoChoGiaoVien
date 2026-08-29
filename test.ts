c6d7823ec103c2e610885416dbc81f4a4777df11
DuongThanhTaii <duongthanhtai13@gmail.com>
2026-08-29 19:28:33 +0700

    chore: remove test files

--- test.ts
+++
@@ -1,1 +0,0 @@
-import { createClient } from '@supabase/supabase-js'; import dotenv from 'dotenv'; dotenv.config({path: '.env.local'}); const supabase = createClient(process.env.NEXT_PUBLIC_SUPABASE_URL, process.env.SUPABASE_SERVICE_ROLE_KEY); supabase.from('class_invitations').select('*').then(console.log);  
